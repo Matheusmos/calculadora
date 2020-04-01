080ed17eb916edf4314c83894a7fa6e83a0986e9
Matheus Oliveira <matheus.engsoftware@gmail.com>
2020-04-01 17:56:37 -0300

    Update
    Uma atualização, apresenta alguns bugs, mas serão corrigidos no prox commit (Este código é apenas uma tarefa, sendo feita durante uma pendamia, não julgue minha organização).

--- App.js
+++ App.js
@@ -6,28 +6,70 @@ export default class App extends Component {
   
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      resultText: ""
+    }
+  }
+  calculateResult(){
+    const text = this.state.resultText
+  }
+
+  buttonPressed(text){
+     
+    console.log(text)
+
+    if(text == '='){
+      return this.calculateResult()
+    }
+    this.setState({
+      resultText: this.state.resultText+text
+    })
+  }
+
+  operate(operation) {
+    switch(operation){
+      case 'D':
+        let text = this.state.resultText.split('')
+        text.pop()
+        this.setState({
+          resultText: text.join('')
+        })
+        break
+      case '+':
+      case '-':
+      case 'X':
+      case '/':
+        if(this.state.text == ""){
+          return
+        }
+        this.setState({
+          resultText: this.state.resultText + operation
+        })
+
+    }
   }
 
   render() {
 
     let rows = []
-    let nuns = [[1,2,3] , [4,5,6] , [7,8,9] , [0, '.', '=']]
+    let nums = [[1,2,3] , [4,5,6] , [7,8,9] , [0, '.', '=']]
     
     for(let i = 0; i < 4; i++){
       let row = []
       for(let j = 0; j < 3; j++){
-        row.push(<TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>{nuns[i][j]}</Text>
-        </TouchableOpacity>)
+        row.push(
+        <TouchableOpacity onPress={() => this.buttonPressed(nums[i][j])} style={styles.btn}>
+          <Text style={styles.btnText}>{nums[i][j]}</Text>
+        </TouchableOpacity>
+        )
         }
         rows.push(<View style={styles.row}>{row}</View>)
       }
 
-      let operations = ['+', '-', 'X', '/']
+      let operations = ['D','+', '-', 'X', '/']
       let ops = []
-      for(let i = 0; i < 4; i++){
-        ops.push(<TouchableOpacity style={styles.btn}>
+      for(let i = 0; i <= 4; i++){
+        ops.push(<TouchableOpacity style={styles.btn} onPress={() => this.operate(operations[i])}>
           <Text style={[styles.btnText, styles.white]}>{operations[i]}</Text>
         </TouchableOpacity>)
       }
@@ -36,7 +78,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.result}>
-            <Text style={styles.resultText}>11*11</Text>
+    <Text style={styles.resultText}>{this.state.resultText}</Text>
         </View>
         <View style={styles.calculation}>
             <Text style={styles.calculationText}>121</Text>
